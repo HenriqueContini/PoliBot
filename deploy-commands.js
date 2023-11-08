8ab7347784f3015e8a5878b5f82421df48f4e7b2
HenriqueContini <henrique.coutine@gmail.com>
2023-11-08 18:06:47 -0300

    Fix: Native Modules

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,7 @@
 const { REST, Routes } = require("discord.js");
 require("dotenv").config();
-const fs = require("node:fs");
-const path = require("node:path");
+const fs = require("fs");
+const path = require("path");
 
 const commands = [];
 const foldersPath = path.join(__dirname, "commands");
