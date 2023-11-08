419bb73f615a347a2195096caa537b5fb2eee587
HenriqueContini <henrique.coutine@gmail.com>
2023-11-08 17:47:10 -0300

    Chore: Global Deploy Config

--- deploy-commands.js
+++ deploy-commands.js
@@ -36,10 +36,7 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN);
     );
 
     const data = await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.CLIENT_ID,
-        process.env.GUILD_ID
-      ),
+      Routes.applicationCommands(process.env.CLIENT_ID),
       { body: commands }
     );
 
